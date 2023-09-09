6e36d11b34bcddb1d99dd35efe36f7b39225137d
Sikander Ahmad <sikanderahmad930@gmail.com>
2023-09-09 17:03:03 +0500

    dice roller

--- dice_app/lib/gradient_container.dart
+++ dice_app/lib/gradient_container.dart
@@ -1,29 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:dice_app/dice_roller.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer({super.key, required this.colors});
+
+  final List<Color> colors;
+
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 44, 21, 96),
-            Color.fromARGB(255, 139, 126, 170),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          colors: colors,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-            fontSize: 28,
-            color: Colors.white,
-          ),
-        ),
-      ),
+      child: const Center(child: DiceRoller()),
     );
   }
 }
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer(this.color1,this.color2,{super.key});
+
+//   final Color color1;
+//   final Color color2;
+
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: [color1,color2],
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: const Center(
+//         child: StyledText('Hello Wo,s'),
+//       ),
+//     );
+//   }
+// }
--- dice_app/lib/main.dart
+++ dice_app/lib/main.dart
@@ -4,7 +4,11 @@ import 'package:dice_app/gradient_container.dart';
 void main() {
   runApp(
     const MaterialApp(
-      home: Scaffold(body: GradientContainer()),
+      home: Scaffold(
+          body: GradientContainer(colors: [
+        Color.fromARGB(255, 44, 21, 96),
+        Color.fromARGB(255, 139, 126, 170),
+      ])),
     ),
   );
 }

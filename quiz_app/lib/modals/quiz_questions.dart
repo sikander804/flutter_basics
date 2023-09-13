0364d51095768c3d599c5a5aaa2f440481fb2b35
Sikander Ahmad <sikanderahmad930@gmail.com>
2023-09-14 00:16:42 +0500

    dummy data with UI

---
+++ quiz_app/lib/modals/quiz_questions.dart
@@ -0,0 +1,12 @@
+class QuizQuestion {
+  QuizQuestion(this.text, this.answers); // constructor
+
+  final String text;
+  final List<String> answers;
+
+  List<String> getShuffledAnswers() {
+    final shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
+}
---
+++ quiz_app/lib/quiz.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/questions_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
+
+  // @override
+  // void initState() {
+  //   activeScreen = StartScreen(switchScreen);
+  //   super.initState();
+  // }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = const QuestionsScreen();
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 4, 4, 4),
+                Color.fromARGB(255, 156, 149, 146)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}

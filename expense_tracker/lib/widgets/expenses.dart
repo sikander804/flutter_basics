14dec31885e9174419cb9e0a2f8aaa06c5413cbb
Sikander Ahmad <sikanderahmad930@gmail.com>
2023-09-22 00:13:34 +0500

    started app for expense tracker

---
+++ expense_tracker/lib/widgets/expenses.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Buy Ticket',
+      amount: 10.99,
+      date: DateTime.now(),
+      category: Category.travel,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Expense Tracker'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          const Text('The Chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          )
+        ],
+      ),
+    );
+  }
+}
